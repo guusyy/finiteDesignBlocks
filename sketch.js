1d4238e78ce3aa9c8ca0cde3ff9558f52fecc788
Guus Noij <gdnoij@Guuss-MacBook-Pro.local>
2020-09-16 16:31:06 +0200

    Blocks can now calculate their own points and save them to the class data.

--- sketch.js
+++ sketch.js
@@ -10,7 +10,10 @@ function setup(){
     strokeWeight(5);
     
     let firstSquare = new square(10,5,40,50,50);
+    firstSquare.calculateMyPoints();
     firstSquare.drawMyself();
+
+    console.log(firstSquare.points)
 }
 
 function draw(){
--- square.js
+++ square.js
@@ -6,10 +6,37 @@ class square {
         this.startPointX = startPointX,
         this.startPointY = startPointY
 
-        this.points = []
+        this.points = [[],[],[],[]]
 
         // this.calculatePoints();
     };
+
+    calculateMyPoints(){
+        for (let i = 0; i < this.amountOfPointsX; i++) {
+            this.points[0].push({
+                x:this.startPointX + this.spacing * i, 
+                y:this.startPointY
+            });
+        }
+        for (let i = 0; i < this.amountOfPointsY; i++) {
+            this.points[1].push({
+                x: this.startPointX + this.spacing * (this.amountOfPointsX - 1), 
+                y:this.startPointY + this.spacing * i
+            });
+        }
+        for (let i = 0; i < this.amountOfPointsX; i++) {
+            this.points[2].push({
+                x: this.startPointX + this.spacing * (this.amountOfPointsX - 1) - this.spacing * i,
+                y: this.startPointY + this.spacing * (this.amountOfPointsY - 1)
+            });
+        }
+        for (let i = 0; i < this.amountOfPointsY; i++) {
+            this.points[3].push({
+                x: this.startPointX, 
+                y: this.startPointY + this.spacing * (this.amountOfPointsY - 1) - this.spacing * i
+            });
+        }
+    }
 
     drawMyself(){
         console.log('drawing myself');
@@ -18,19 +45,19 @@ class square {
 
         beginShape(POINTS);
             for (let i = 0; i < this.amountOfPointsX; i++) {
-                vertex(this.startPointX + this.spacing * i, this.startPointY);
+                vertex(this.points[0][i].x,this.points[0][i].y);
             }
             stroke(255,0,0);
             for (let i = 0; i < this.amountOfPointsY; i++) {
-                vertex(this.startPointX + this.spacing * (this.amountOfPointsX - 1), this.startPointY + this.spacing * i);
+                vertex(this.points[1][i].x,this.points[1][i].y);
             }
             stroke(0,255,0);
             for (let i = 0; i < this.amountOfPointsX; i++) {
-                vertex(this.startPointX + this.spacing * (this.amountOfPointsX - 1) - this.spacing * i, this.startPointY + this.spacing * (this.amountOfPointsY - 1));
+                vertex(this.points[2][i].x,this.points[2][i].y);
             }
             stroke(0,0,255);
             for (let i = 0; i < this.amountOfPointsY; i++) {
-                vertex(this.startPointX, this.startPointY + this.spacing * (this.amountOfPointsY - 1) - this.spacing * i);
+                vertex(this.points[3][i].x,this.points[3][i].y);
             }
         endShape();
     };
